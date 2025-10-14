969ae0329ce4de39d66a105ca3fbba9ec09c834f
Safya174 <safyabdelsalam84@gmail.com>
2025-10-14 23:40:54 +0300

    fix: add localStorage support and update logic

--- Js/main.js
+++ Js/main.js
@@ -18,25 +18,44 @@ function showinfo(){
       alert(Student[i].Name + "  " +Student[i].grade);
    }
 }*/
+
 var productName = document.getElementById("fn");
 var productPrice = document.getElementById("pc");
 var productCategory = document.getElementById("pb");
 var productdesc = document.getElementById("pd");
 var allProudct = [];
+if(localStorage.getItem("allProudct") != null)
+{
+   allProudct = JSON.parse(localStorage.getItem("allProudct"));
+   displayAllProduct();
+}
+var curretIndex =  -1 ;
 function addNewElement()
 {
-  var  product ={
+   var  product ={
      name: productName.value,
      Price: productPrice.value,
      category: productCategory.value,
      productdesc : productdesc.value
       
   }
+  if(curretIndex != -1) 
+  {
+    
+    allProudct[curretIndex] = product;
+    
+  }
+  else{
   allProudct.push(product);
-   clearForm();
-   displayAllProduct();
+  localStorage.getItem("allProudct",JSON.stringify(allProudct));
+  }
+  document.getElementById("Update").innerHTML = "Add Element" ;
+  clearForm();
+  localStorage.setItem("allProudct", JSON.stringify(allProudct));
 
-
+  displayAllProduct();
+  curretIndex = -1;
+  
 }
 function clearForm()
 {
@@ -56,9 +75,27 @@ function displayAllProduct()
          <td>${ allProudct[i].Price }</td>
          <td>${ allProudct[i].category }</td>
          <td>${ allProudct[i].productdesc }</td>
+         <td> <button class="btn btn-danger" onclick="deleteProduct(${i})"  >Delete</button></td>
+         <td> <button class="btn btn-warning" onclick= "updateproduct(${i})"  >Update</button></td>
           </tr>`
   }
   document.getElementById("Data").innerHTML = cartona;
 }
+function deleteProduct(index){
+ allProudct.splice(index,1);
+ displayAllProduct();
+ localStorage.setItem("allProudct", JSON.stringify(allProudct));
+}
+function updateproduct(index){
+   productName.value = allProudct[index].name;
+   productPrice.value = allProudct[index].Price;
+   productCategory.value = allProudct[index].category;
+   productdesc.value = allProudct[index].productdesc;
+   curretIndex = index;
+   document.getElementById("Update").innerHTML = "Update" ;
+   localStorage.setItem("allProudct", JSON.stringify(allProudct));
+
+}
+
 
 
