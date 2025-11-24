bad52da3df5c986de22b8f2be41e3847e0b8805e
Safya174 <safyabdelsalam84@gmail.com>
2025-11-24 02:21:24 +0200

    Replace old version with updated version

--- Js/main.js
+++ Js/main.js
@@ -1,23 +1,4 @@
-/*var firstName = document.getElementById("fn");    
-var LastName = document.getElementById("ln");   
 
-function sayName(){
-  window.alert("Hello"+" "+firstName.value +"  "+LastName.value) ;
-
-
-}*/
-/*var Student = [
-  {Name:"Safya",grade:"A+"},
-  {Name:"Ahmed",grade:"A"},
-   {Name:"yasir",grade:"B"},
-
-]
-function showinfo(){
-   for(i=0;i < Student.length;i++)
-   {
-      alert(Student[i].Name + "  " +Student[i].grade);
-   }
-}*/
 
 var productName = document.getElementById("fn");
 var productPrice = document.getElementById("pc");
@@ -32,6 +13,10 @@ if(localStorage.getItem("allProudct") != null)
 var curretIndex =  -1 ;
 function addNewElement()
 {
+ if(validateName() & validateprice() & validatecategroy() & validatedesc() )
+{
+
+
    var  product ={
      name: productName.value,
      Price: productPrice.value,
@@ -56,6 +41,24 @@ function addNewElement()
   displayAllProduct();
   curretIndex = -1;
   
+}
+else{
+  if(validateName() != true)
+  {
+    alert("The Name is not valid");
+  }
+  else if(validateprice() != true)
+  {
+     alert("The price is Not valid");
+  }
+  else if(validatecategroy() != true)
+  {
+     alert("The categroy is Not valid");
+  }
+  else{
+     alert("The descradtion is not valid");
+  }
+}
 }
 function clearForm()
 {
@@ -91,11 +94,49 @@ function updateproduct(index){
    productPrice.value = allProudct[index].Price;
    productCategory.value = allProudct[index].category;
    productdesc.value = allProudct[index].productdesc;
-   curretIndex = index;
+   curretIndex = index; 
    document.getElementById("Update").innerHTML = "Update" ;
    localStorage.setItem("allProudct", JSON.stringify(allProudct));
 
 }
+
+function search(trem){
+  
+  var cartona = "";
+  for(var i=0;i < allProudct.length;i++)
+  {
+    if( allProudct[i].name.toLocaleLowerCase().indexOf(trem.toLocaleLowerCase()) == 0 || allProudct[i].category.toLocaleLowerCase().includes(trem.toLocaleLowerCase())  ){
+        cartona += `<tr>
+         <td>${ allProudct[i].name }</td>
+         <td>${ allProudct[i].Price }</td>
+         <td>${ allProudct[i].category }</td>
+         <td>${ allProudct[i].productdesc }</td>
+         <td> <button class="btn btn-danger" onclick="deleteProduct(${i})"  >Delete</button></td>
+         <td> <button class="btn btn-warning" onclick= "updateproduct(${i})"  >Update</button></td>
+          </tr>`
+    }
+  }
+  document.getElementById("Data").innerHTML = cartona;
+
+}
+function validateName(){
+  var regexpname = /^[a-z]{3,7}$/i
+   return  regexpname.test(productName.value);
+}
+function validateprice(){
+  var regexprice = /^[0-9]{1,10}[\$]?$/i
+  return regexprice.test(productPrice.value);
+}
+function validatecategroy(){
+  var regexpcategory =   /^[a-z]{3,8}$/i
+  return  regexpcategory.test(productCategory.value);
+}
+function validatedesc(){
+  var regexdesc =  /^[a-z]{3,100}$/i
+  return regexdesc.test(productdesc.value);
+
+}
+
 
 
 
